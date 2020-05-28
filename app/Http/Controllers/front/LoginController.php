0be37e05cecc1c05d6c9ea8759588ec726dea58a
ronypermadi <ronnypermadi1@gmail.com>
2020-05-28 20:00:10 +0700

    dashboard and management orders

--- app/Http/Controllers/front/LoginController.php
+++ app/Http/Controllers/front/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\front;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Order;
 
 class LoginController extends Controller
 {
@@ -36,7 +37,12 @@ class LoginController extends Controller
     }
 
     public function dashboard(){
-        return view('front.ecommerce.dashboard');
+        $orders = Order::selectRaw('COALESCE(sum(CASE WHEN status = 0 THEN subtotal END), 0) as pending, 
+        COALESCE(count(CASE WHEN status = 3 THEN subtotal END), 0) as shipping,
+        COALESCE(count(CASE WHEN status = 4 THEN subtotal END), 0) as completeOrder')
+        ->where('customer_id', auth()->guard('customer')->user()->id)->get();
+        
+        return view('front.ecommerce.dashboard', compact('orders'));
     }
     public function logout(){
         auth()->guard('customer')->logout(); //JADI KITA LOGOUT SESSION DARI GUARD CUSTOMER
--- app/Order.php
+++ app/Order.php
@@ -7,8 +7,33 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $guarded = [];
+    
+    protected $appends = ['status_label'];
+
+    public function getStatusLabelAttribute(){
+        if ($this->status == 0) {
+            return '<span class="badge badge-secondary">Baru</span>';
+        } elseif ($this->status == 1) {
+            return '<span class="badge badge-primary">Dikonfirmasi</span>';
+        } elseif ($this->status == 2) {
+            return '<span class="badge badge-info">Proses</span>';
+        } elseif ($this->status == 3) {
+            return '<span class="badge badge-warning">Dikirim</span>';
+        }
+        return '<span class="badge badge-success">Selesai</span>';
+    }
+
     //MEMBUAT RELASI KE MODEL DISTRICT.PHP
     public function district(){
         return $this->belongsTo(District::class);
     }
+    //MEMBUAT RELASI KE TABLE ORDER_DETAILS DENGAN JENIS RELASI ONE TO MANY
+    public function details(){
+        return $this->hasMany(OrderDetail::class);
+    }
+
+    public function payment(){
+        return $this->hasOne(Payment::class);
+    }
+
 }
